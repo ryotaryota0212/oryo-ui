eeaea416cd6ce4db20409087e238c7c6078eb493
大谷瞭太 <rooya@fourmix.co.jp>
2024-12-10 12:00:48 +0900

    一旦コミット

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import { createPinia } from 'pinia'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
-import BootstrapVue3 from 'bootstrap-vue-3';
-
 import App from './App.vue'
 import router from './router'
 
